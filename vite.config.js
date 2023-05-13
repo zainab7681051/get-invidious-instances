aa97293db93157ed55c02c66b730e21f88f5eb0e
zainab7681051 <zainab7681051@gmail.com>
2023-05-14 00:48:48 +0300

    build target value changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/get-invidious-instances/",
   plugins: [vue()],
-})
+  build: {
+    target: ["es2020", "edge88", "firefox78", "chrome87", "safari12"],
+  },
+});
